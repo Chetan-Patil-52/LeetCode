61487847ae68042a9c4df9c928b2bf9c2487431d
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-01-16 18:54:12 +0530

    Update 2425. Bitwise XOR of All Pairings.cpp

--- Year_2025/January/Day_16/2425. Bitwise XOR of All Pairings.cpp
+++ Year_2025/January/Day_16/2425. Bitwise XOR of All Pairings.cpp
@@ -27,3 +27,49 @@ public:
         return sum;
     }
 };
+
+
+
+// Approch - 2 (Optimal )
+
+class Solution {
+public:
+    // Optimal Approach 
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int  m = nums2.size();
+        
+        int xOR1 = 0;
+        int xOR2 = 0;
+        for(int num : nums1) xOR1 ^= num;
+        for(int num : nums2) xOR2 ^= num;
+     
+
+        return (n%2==1 ? xOR2 : 0) ^ (m%2==1 ? xOR1 : 0);
+    }
+};
+
+// OR 
+class Solution {
+public:
+    // Optimal Approach 
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int xOR = 0;
+        if(m%2!=0){
+            for(int num : nums2){
+                xOR ^= num;
+            }
+        }
+
+        if(n%2!=0){
+            for(int num : nums1){
+                xOR ^= num;
+            }
+        }
+
+        return xOR;
+    }
+};
