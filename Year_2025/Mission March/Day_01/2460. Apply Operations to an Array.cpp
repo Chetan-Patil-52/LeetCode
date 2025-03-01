659f8ac1a434d7c34a87fc613767bb61ddfc9522
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-03-01 14:05:41 +0530

    Update 2460. Apply Operations to an Array.cpp

--- Year_2025/Mission March/Day_01/2460. Apply Operations to an Array.cpp
+++ Year_2025/Mission March/Day_01/2460. Apply Operations to an Array.cpp
@@ -1,1 +1,26 @@
 // Code
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        // Step - 1 (Transformation)
+        for(int i=0;i<n;i++){
+            if(i != n-1 && nums[i]==nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+
+        // Step - 2 (Moving Zeros)
+        int idx = 0;
+        vector<int> result(n,0);
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                result[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        return result;
+    }
+};
