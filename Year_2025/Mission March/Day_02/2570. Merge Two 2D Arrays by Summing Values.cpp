37973ab1afe1ecd806c35f91f91783f510ec9d13
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-03-02 21:03:46 +0530

    Update 2570. Merge Two 2D Arrays by Summing Values.cpp

--- Year_2025/Mission March/Day_02/2570. Merge Two 2D Arrays by Summing Values.cpp
+++ Year_2025/Mission March/Day_02/2570. Merge Two 2D Arrays by Summing Values.cpp
@@ -24,3 +24,43 @@ public:
         return result;
     }
 };
+
+// Optimal Approach - Two pointer 
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int m = nums1.size(),n = nums2.size();
+        // step - 1 keep a loop until i < n and j < m
+        int i = 0;
+        int j = 0;
+        vector<vector<int>> result;
+        while(i<m && j<n){
+            if(nums1[i][0] < nums2[j][0]){
+                result.push_back(nums1[i]);
+                i++;
+            }else if(nums2[j][0] < nums1[i][0]){
+                result.push_back(nums2[j]);
+                j++;
+            }else{
+                result.push_back({nums1[i][0],nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
+        }
+
+        while(i<m){
+            result.push_back(nums1[i]);
+            i++;
+        }
+
+        while(j<n){
+            result.push_back(nums2[j]);
+            j++;
+        }
+        // if both are same increment both of them else eitherr one of them
+        // put the result in result array
+
+        return result;
+
+    }
+};
