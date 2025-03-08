e74c9edf5d788c3c3b94e0e080283f91480e022d
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-03-09 00:10:01 +0530

    Update 2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp

--- Year_2025/Mission March/Day_08/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
+++ Year_2025/Mission March/Day_08/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -1,1 +1,18 @@
-//Code
+//Bruteforce Code TC: O(N^2)
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int n = blocks.length();
+        int result = INT_MAX;
+        for(int i=0;i<=n-k;i++){
+            int w = 0;
+            for(int j=i;j-i+1<=k;j++){
+                if(blocks[j]=='W'){
+                    w++;
+                }
+            }
+            result = min(result,w);
+        }
+        return result;
+    }
+};
