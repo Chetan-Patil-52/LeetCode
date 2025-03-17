ed13e0a0a7e9d391f52cc48ba22297198cfd254d
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-03-17 19:21:06 +0530

    Update 2206. Divide Array Into Equal Pairs.cpp

--- Year_2025/Mission March/Day_17/2206. Divide Array Into Equal Pairs.cpp
+++ Year_2025/Mission March/Day_17/2206. Divide Array Into Equal Pairs.cpp
@@ -17,3 +17,71 @@ public:
         return true;
     }
 };
+
+// Using Set 
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> st;
+        for(auto& ele : nums){
+            if(st.count(ele)){
+                st.erase(ele);
+            }else{
+                st.insert(ele);
+            }
+        }
+        return st.empty();
+    }
+};
+
+// Using Vector 
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        vector<bool> paired(501,true);
+
+        for(int &x : nums){
+            paired[x] = !paired[x];
+        }
+
+        for(int i=0;i<501;i++){
+            if(paired[i] != true){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
+
+// Sort and Check 
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int i=1;i<n;i+2){
+            if(nums[i] != nums[i-1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+// Sort and XOR
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums),end(nums));
+
+        for(int i=1;i<n;i+=2){
+            if(nums[i] ^ nums[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
