a43b51c05b50a4617b16547d2da5bfffbf2413f3
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-04-14 11:37:24 +0530

    Update 1922. Count Good Numbers.cpp

--- Year_2025/April Bytes/Day_13/1922. Count Good Numbers.cpp
+++ Year_2025/April Bytes/Day_13/1922. Count Good Numbers.cpp
@@ -1,25 +1,26 @@
-//Approach (Using Maths)
-//T.C : O(log(n))
-//S.C : O(1) but note that we use O(log(n)) system stack space of recursion
 class Solution {
 public:
-    const int M = 1e9+7;
-    int findPower(long long a, long long b) {
-        if(b == 0) {
-            return 1;
-        }   
 
-        long long half = findPower(a, b/2);
+    const int M = 1e9+7;
+    // eg . 2^5 thus a=2,b=5
+    // Binary exponential
+    int findPower(long long a,long long b){
+        if(b==0){
+            return 1;
+        }
+
+        long long half = findPower(a,b/2);
         long long result = (half * half) % M;
 
-        if(b % 2 == 1) {
-            result = (result * a) % M;
+        if(b % 2 == 1){
+            result =(a * result) % M;
         }
 
         return result;
     }
 
     int countGoodNumbers(long long n) {
-        return (long long)findPower(5, (n+1)/2) * findPower(4, n/2) % M;
+        // Here 5 coz there are 5 even numbers including 0 from 1-10 and 4 Prime numbers
+        return (long long)findPower(5,(n+1)/2) * findPower(4,n/2) % M;
     }
 };
