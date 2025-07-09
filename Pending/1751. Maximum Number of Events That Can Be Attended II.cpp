342e7a471e3af4f45d6161c63c6c90fe7efa0b3c
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-07-09 12:07:56 +0530

    Update 1751. Maximum Number of Events That Can Be Attended II.cpp

--- Pending/1751. Maximum Number of Events That Can Be Attended II.cpp
+++ Pending/1751. Maximum Number of Events That Can Be Attended II.cpp
@@ -1,3 +1,111 @@
+// TC : O(n+1) * O(k+1) * O(n)-Liner Search
+// Gives TLE (69/70)
+class Solution {
+public:
+    int n;
+    int solve(vector<vector<int>>& events, int idx, int k){
+        if(idx >= n || k == 0){
+            return 0;
+        }
+
+        int start = events[idx][0];
+        int end   = events[idx][1];
+        int value = events[idx][2];
+
+        int skip = solve(events,idx+1,k);
+
+        int j = idx+1;
+        for( ; j<n;j++){
+            if(events[j][0] > events[idx][1]){
+                break;
+            }
+        }
+        int take = value + solve(events,j,k-1);
+        return max(take,skip);
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        n = events.size();
+        sort(begin(events),end(events));
+
+        return solve(events,0,k);
+    }
+};
+
+// TC : O(n+1) * O(k+1) * O(nlogn)-Liner Search
+// Gives TLE (68/70)
+
+class Solution {
+public:
+    int n;
+    vector<vector<int>> t;
+    int solve(vector<vector<int>>& events, int idx, int k){
+        if(idx >= n || k == 0){
+            return 0;
+        }
+
+        if(t[idx][k] != -1){
+            return t[idx][k];
+        }
+
+        int start = events[idx][0];
+        int end   = events[idx][1];
+        int value = events[idx][2];
+
+        int skip = solve(events,idx+1,k);
+
+        vector<int> temp = {end,INT_MAX,INT_MAX};
+        int j = upper_bound(events.begin()+idx+1,events.end(),temp) - events.begin();
+
+        int take = value + solve(events,j,k-1);
+        return t[idx][k] = max(take,skip);
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        n = events.size();
+        sort(begin(events),end(events));
+        t.resize(n+1, vector<int>(k+1,-1));
+        return solve(events,0,k);
+    }
+};
+// with predefined/Custom comparator
+class Solution {
+public:
+    int n;
+    vector<vector<int>> t;
+
+    int solve(vector<vector<int>>& events, int idx, int k){
+        if(idx >= n || k == 0){
+            return 0;
+        }
+
+        if(t[idx][k] != -1){
+            return t[idx][k];
+        }
+
+        int value = events[idx][2];
+
+        // Skip current event
+        int skip = solve(events, idx + 1, k);
+
+        // Binary search: find first event with start time > current event's end
+        int endTime = events[idx][1];
+        int j = upper_bound(events.begin(), events.end(), endTime,
+            [](int val, const vector<int>& event) {
+                return val < event[0];
+            }) - events.begin();
+
+        int take = value + solve(events, j, k - 1);
+        return t[idx][k] = max(take, skip);
+    }
+
+    int maxValue(vector<vector<int>>& events, int k) {
+        n = events.size();
+        sort(events.begin(), events.end());
+        t.assign(n, vector<int>(k + 1, -1));
+        return solve(events, 0, k);
+    }
+};
+
+
 //Approach-3 (Without doing any linear or binary search) - Simply keep track of last taken event
 class Solution {
 public:
