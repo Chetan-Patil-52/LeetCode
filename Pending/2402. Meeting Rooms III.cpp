2bcbe29616617a4003eb490b5bc3fe77ff8f036e
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-07-11 21:21:36 +0530

    Update 2402. Meeting Rooms III.cpp

--- Pending/2402. Meeting Rooms III.cpp
+++ Pending/2402. Meeting Rooms III.cpp
@@ -46,3 +46,59 @@ public:
         return resultRoom;
     }
 };
+
+
+// Using priority queues
+class Solution {
+public:
+    typedef pair<long long, int> P;
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        // Using 2 priorirty queues
+        int m = meetings.size();
+        sort(begin(meetings),end(meetings));
+        vector<int> roomUsedCount(n,0);
+        priority_queue<P,vector<P>,greater<P>> usedRooms;
+        priority_queue<int,vector<int>,greater<int>> availableRooms;
+
+        for(int room = 0; room<n;room++){
+            availableRooms.push(room);
+        }
+
+        for(vector<int>& meet : meetings){
+            int start = meet[0];
+            int end   = meet[1];
+            int duration = end-start;
+
+            while(!usedRooms.empty() && usedRooms.top().first <= start){
+                int room = usedRooms.top().second;
+                usedRooms.pop();
+                availableRooms.push(room);
+            }
+
+            if(!availableRooms.empty()){
+                int room = availableRooms.top();
+                availableRooms.pop();
+                usedRooms.push({end,room});
+                roomUsedCount[room]++;
+            }else{
+                int room = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+
+                usedRooms.pop();
+                usedRooms.push({endTime + duration,room});
+                roomUsedCount[room]++;
+            }
+        }
+
+        int maxUse = 0;
+        int resultRoom = -1;
+
+        for(int room=0;room<n;room++){
+            if(roomUsedCount[room] > maxUse){
+                maxUse = roomUsedCount[room];
+                resultRoom = room;
+            }
+        }
+        return resultRoom;
+    }
+};
