8eede755a4ddd240495e8af15150b6c691696ae7
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-07-27 15:37:57 +0530

    Update 2210. Count Hills and Valleys in an Array.cpp

--- Easy_Problems/2210. Count Hills and Valleys in an Array.cpp
+++ Easy_Problems/2210. Count Hills and Valleys in an Array.cpp
@@ -52,3 +52,46 @@ public:
     }
 };
 
+
+// Worked
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> left(n,0);
+        left[0] = nums[0];
+        for(int i=1;i<n-1;i++){
+            if(nums[i]==nums[i-1]){
+                left[i]=left[i-1];
+            }else{
+                left[i]=nums[i-1];
+            }
+        }
+
+        vector<int> right(n,0);
+        right[n-1] = nums[n-1];
+        for(int i=n-2;i>0;i--){
+            if(nums[i]==nums[i+1]){
+                right[i]=right[i+1];
+            }else{
+                right[i]=nums[i+1];
+            }   
+        }
+
+        int v = 0;
+        int h = 0;
+        for(int i=1;i<n-1;i++){
+            
+            while(i<n-1 && nums[i]==nums[i+1]){
+                i++;
+            }
+
+            if(i>= n-1) break;
+
+            if(nums[i] < left[i] && nums[i] <right[i]) v++;
+            if(nums[i] > left[i] && nums[i] >right[i]) h++;
+        }
+
+        return v+h;
+    }
+};
